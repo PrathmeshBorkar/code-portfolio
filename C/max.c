6fa4ce8009d84c879541e04d7267577faca233b6
PrathmeshBorkar <prathmeshborkar34@gmail.com>
2026-07-06 18:21:57 +0530

    Added 3 programs in C

---
+++ C/max.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int findMax(int a, int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int n1, n2, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &n1, &n2);
+
+    max = findMax(n1, n2);
+
+    printf("Maximum number is: %d\n", max);
+
+    return 0;
+}
