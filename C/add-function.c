12a88391236dd8bef4ee6b07aff701ce8e3c44b3
PrathmeshBorkar <prathmeshborkar34@gmail.com>
2026-07-07 22:54:56 +0530

    Add multi-file C example: function prototype and definition across files

---
+++ C/add-function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int Add()
+{
+    int a,b,c;
+
+    printf("Enter first number : ");
+    scanf("%d",&a);
+    
+    printf("Enter second number : ");
+    scanf("%d",&b);
+
+    c=a+b;
+
+    return c;
+}
---
+++ C/main-multifile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int Add();
+
+int main()
+{
+    int result;
+    
+    result = Add();
+    printf("Addition : %d",result);
+
+    return 0;
+}
